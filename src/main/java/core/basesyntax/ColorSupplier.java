bc0e41ce2f7f6a7bc90fe7a4f17d4b90a4094927
kromch <94765048+kromch@users.noreply.github.com>
2023-01-11 01:03:25 +0200

    Update ColorSupplier.java

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
+    enum Colors {
+        Aquamarine,
+        Azure,
+        BurlyWood,
+        CadetBlue,
+        Gainsboro,
+        Gold,
+        Gray,
+        Khaki,
+        LawnGreen,
+        LightGreen,
+        LightSkyBlue,
+        Linen,
+        MediumOrchid,
+        MediumPurple,
+        MistyRose,
+        Olive,
+        OliveDrab,
+        Orange,
+        OrangeRed,
+        Orchid,
+        PaleTurquoise,
+        Peru,
+        Pink,
+        Plum,
+        RoyalBlue,
+        SandyBrown,
+        SeaGreen,
+        SteelBlue;
+    }
+
     public String getRandomColor() {
-        return null;
+        Random random = new Random();
+        Colors[] colors = Colors.values();
+        return colors[random.nextInt(colors.length - 1)].toString();
     }
 }
